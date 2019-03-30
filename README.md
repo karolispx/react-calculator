a9504685912a5f049f7ed43f4396868dbaa57cc4
Karolis Pliauskys MB <kpliauskys@threefoldsystems.com>
2019-03-30 07:32:19 +0000

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Learning ReactJS 1 - Simple Calculator 
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/index.js
+++ src/index.js
@@ -1,12 +1,173 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Calculator extends React.Component {
+    // Calculator
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: '0',
+            previousKey: 'operator'
+        };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        // Event listener to update result input when this changes
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    // Render button
+    renderButton(value, type) {
+        return (
+            <button className="calculator-button" onClick={() => this.handleClick(value, type)}>
+                {value}
+            </button>
+        );
+    }
+
+    // Render result
+    renderResult() {
+        return (
+            <input type="text" className="calculator-result" value={this.state.result} onChange={this.handleChange} readOnly/>
+        );
+    }
+
+    // Handle input change
+    handleChange(event) {
+        this.setState({result: event.target.value});
+    }
+
+    // Click on a button
+    handleClick(value, type) {
+        let previousKey = this.state.previousKey;
+        let oldResult = this.state.result;
+
+        let newKey = type;
+        let newResult = oldResult;
+
+        if ( type === 'operator' && previousKey !== 'operator' ) {
+            newResult = oldResult + value;
+        } else if ( type === 'clear' ) {
+            newResult = '0';
+            newKey = 'operator';
+        } else if ( type === 'number' ) {
+            if ( oldResult === '0' ) {
+                newResult = value;
+            } else {
+                newResult = oldResult + '' + value;
+            }
+        } else if ( type === 'equal' && previousKey === 'number' ) {
+            newResult = eval(oldResult); // Probably not very safe.
+        }
+
+        this.setState({
+            result: newResult,
+            previousKey: newKey
+        });
+    }
+
+    // Render calculator
+    render() {
+        return (
+            <div className="calculator-content">
+                <div className="row">
+                    <div className="column">
+                        <h1>Calculator</h1>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column">
+                        {this.renderResult()}
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column">
+                        {this.renderButton('7', 'number')}
+                    </div>
+
+                    <div className="column">
+                        {this.renderButton('8', 'number')}
+                    </div>
+
+                    <div className="column">
+                        {this.renderButton('9', 'number')}
+                    </div>
+
+                    <div className="column column-helper">
+                        {this.renderButton('*', 'operator')}
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column">
+                        {this.renderButton('4', 'number')}
+                    </div>
+
+                    <div className="column">
+                        {this.renderButton('5', 'number')}
+                    </div>
+
+                    <div className="column">
+                        {this.renderButton('6', 'number')}
+                    </div>
+
+                    <div className="column column-helper">
+                        {this.renderButton('/', 'operator')}
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column">
+                        {this.renderButton('1', 'number')}
+                    </div>
+
+                    <div className="column">
+                        {this.renderButton('2', 'number')}
+                    </div>
+
+                    <div className="column">
+                        {this.renderButton('3', 'number')}
+                    </div>
+
+                    <div className="column column-helper">
+                        {this.renderButton('-', 'operator')}
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="column column-danger">
+                        {this.renderButton('C', 'clear')}
+                    </div>
+
+                    <div className="column">
+                        {this.renderButton('0', 'number')}
+                    </div>
+
+                    <div className="column column-helper">
+                        {this.renderButton('=', 'equal')}
+                    </div>
+
+                    <div className="column column-helper">
+                        {this.renderButton('+', 'operator')}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="calculator">
+                <Calculator />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
